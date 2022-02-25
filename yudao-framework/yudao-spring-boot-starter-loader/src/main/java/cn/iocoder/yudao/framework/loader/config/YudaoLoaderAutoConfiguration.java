c87b6bc45930479e8386a8feb9963eeb67745d50
damon.li <damon.li@yamibuy.com>
2022-02-25 12:38:44 +0800

    1. 包名更改

--- yudao-framework/yudao-spring-boot-starter-loader/src/main/java/cn/iocoder/yudao/framework/loader/config/YudaoLoaderAutoConfiguration.java
+++ yudao-framework/yudao-spring-boot-starter-loader/src/main/java/cn/iocoder/yudao/framework/loader/config/YudaoLoaderAutoConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class YudaoCovertAutoConfiguration {
+public class YudaoLoaderAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(SpringUtil.class)
