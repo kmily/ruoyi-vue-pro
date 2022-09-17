10d544f4bbf359d20016806103b1a89280a482b8
heming <dm.he@outlook.com>
2022-09-17 13:22:08 +0800

    update 代码写成一行, basePackages类型换成List

--- yudao-framework/yudao-spring-boot-starter-web/src/main/java/cn/iocoder/yudao/framework/swagger/config/YudaoSwaggerAutoConfiguration.java
+++ yudao-framework/yudao-spring-boot-starter-web/src/main/java/cn/iocoder/yudao/framework/swagger/config/YudaoSwaggerAutoConfiguration.java
@@ -49,7 +49,7 @@ public class YudaoSwaggerAutoConfiguration {
                 // ② 设置扫描指定 package 包下的
                 .select()
                 // 支持扫描多包
-                .apis(t -> Arrays.stream(properties.getBasePackages()).map(RequestHandlerSelectors::basePackage).anyMatch(predicate -> predicate.test(t)))
+                .apis(t -> properties.getBasePackages().stream().map(RequestHandlerSelectors::basePackage).anyMatch(predicate -> predicate.test(t)))
 //                .apis(basePackage("cn.iocoder.yudao.module.system")) // 可用于 swagger 无法展示时使用
                 .paths(PathSelectors.any())
                 .build()
