48440563076fd660f817dd6f4897dc44594fa045
klaokai <573984425@qq.com>
2023-04-24 12:20:56 +0800

    chore: 调整注释

--- yudao-module-infra/yudao-module-infra-biz/src/main/java/cn/iocoder/yudao/module/infra/service/codegen/inner/generator/sql/MySQLDialect.java
+++ yudao-module-infra/yudao-module-infra-biz/src/main/java/cn/iocoder/yudao/module/infra/service/codegen/inner/generator/sql/MySQLDialect.java
@@ -7,23 +7,11 @@ package cn.iocoder.yudao.module.infra.service.codegen.inner.generator.sql;
  */
 public class MySQLDialect implements SQLDialect {
 
-    /**
-     * 封装字段名
-     *
-     * @param name
-     * @return
-     */
     @Override
     public String wrapFieldName(String name) {
         return String.format("`%s`", name);
     }
 
-    /**
-     * 解析字段名
-     *
-     * @param fieldName
-     * @return
-     */
     @Override
     public String parseFieldName(String fieldName) {
         if (fieldName.startsWith("`") && fieldName.endsWith("`")) {
@@ -32,23 +20,11 @@ public class MySQLDialect implements SQLDialect {
         return fieldName;
     }
 
-    /**
-     * 包装表名
-     *
-     * @param name
-     * @return
-     */
     @Override
     public String wrapTableName(String name) {
         return String.format("`%s`", name);
     }
 
-    /**
-     * 解析表名
-     *
-     * @param tableName
-     * @return
-     */
     @Override
     public String parseTableName(String tableName) {
         if (tableName.startsWith("`") && tableName.endsWith("`")) {
--- yudao-module-infra/yudao-module-infra-biz/src/main/java/cn/iocoder/yudao/module/infra/service/codegen/inner/generator/sql/SQLDialect.java
+++ yudao-module-infra/yudao-module-infra-biz/src/main/java/cn/iocoder/yudao/module/infra/service/codegen/inner/generator/sql/SQLDialect.java
@@ -9,29 +9,33 @@ public interface SQLDialect {
 
     /**
      * 封装字段名
-     * @param name
-     * @return
+     *
+     * @param name 字段名称
+     * @return 包装后的名称
      */
     String wrapFieldName(String name);
 
     /**
      * 解析字段名
-     * @param fieldName
-     * @return
+     *
+     * @param fieldName 包装后的名称
+     * @return 解析出来的字段名称
      */
     String parseFieldName(String fieldName);
 
     /**
      * 封装表名
-     * @param name
-     * @return
+     *
+     * @param name 表名称
+     * @return 包装后的名称
      */
     String wrapTableName(String name);
 
     /**
      * 解析表名
-     * @param tableName
-     * @return
+     *
+     * @param tableName 包装后的表名称
+     * @return 解析出来的表名称
      */
     String parseTableName(String tableName);
 }
