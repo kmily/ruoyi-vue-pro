63d9b076259d9ac46ed8ec950c59ca34d5975bc1
吴桥鑫 <1282047358@qq.com>
2023-06-10 16:42:42 +0800

    订单价格错误修改

--- cw-module-trade/src/main/java/com/cw/module/trade/handler/WebSocketHandlerFactory.java
+++ cw-module-trade/src/main/java/com/cw/module/trade/handler/WebSocketHandlerFactory.java
@@ -347,13 +347,15 @@ public class WebSocketHandlerFactory {
         String stopPriceStr = stopPrice == null || stopPrice.compareTo(BigDecimal.ZERO) == 0 ? null : stopPrice.toString();
         String followOrderPriceStr = followOrderPrice == null || 
                 followOrderPrice.compareTo(BigDecimal.ZERO) == 0 ? null : stopPrice.toString();
+        String followOrderQtyStr = followOrderQty == null || 
+                followOrderQty.compareTo(BigDecimal.ZERO) == 0 ? null : stopPrice.toString();
         String reduceOnly = null;
         params.addProperty("symbol", order.getSymbol());
         params.addProperty("side", order.getSide());
         params.addProperty("positionSide", order.getPositionSide());
         params.addProperty("type", order.getType());
         params.addProperty("timeInForce", order.getTimeInForce());
-        params.addProperty("quantity", followOrderQty.toString());
+        params.addProperty("quantity", followOrderQtyStr);
         params.addProperty("price", followOrderPriceStr);
         params.addProperty("reduceOnly", reduceOnly);
         params.addProperty("stopPrice", stopPriceStr);
@@ -366,7 +368,7 @@ public class WebSocketHandlerFactory {
                     PositionSide.format(order.getPositionSide()), //positionSide  持仓方向，单向持仓模式下非必填，默认且仅可填BOTH;在双向持仓模式下必填,且仅可选择 LONG 或 SHORT
                     OrderType.lookup(order.getType()),    // orderType 订单类型 LIMIT, MARKET, STOP, TAKE_PROFIT, STOP_MARKET, TAKE_PROFIT_MARKET, TRAILING_STOP_MARKET
                     TimeInForce.valueOf(order.getTimeInForce()) ,    // timeInForce  有效方法
-                    followOrderQty.toString(),    // quantity     下单数量,使用closePosition不支持此参数。
+                    followOrderQtyStr,    // quantity     下单数量,使用closePosition不支持此参数。
                     followOrderPriceStr, // price    委托价格
                     reduceOnly,   // reduceOnly true, false; 非双开模式下默认false；双开模式下不接受此参数； 使用closePosition不支持此参数。
                     null,   // newClientOrderId 用户自定义的订单号，不可以重复出现在挂单中。如空缺系统会自动赋值。必须满足正则规则 ^[\.A-Z\:/a-z0-9_-]{1,36}$
