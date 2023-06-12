1c3e35a1a48f302f20f4d0c3fda4c68c05642d38
gz3ray <fdola@qq.com>
2023-06-12 18:40:32 +0800

    新增通过名字搜索客户

--- yudao-module-oa/yudao-module-oa-biz/src/main/java/cn/iocoder/yudao/module/oa/controller/app/attendance/AppAttendanceController.java
+++ yudao-module-oa/yudao-module-oa-biz/src/main/java/cn/iocoder/yudao/module/oa/controller/app/attendance/AppAttendanceController.java
@@ -23,6 +23,8 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
+import java.util.List;
+
 import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
 import static cn.iocoder.yudao.framework.operatelog.core.enums.OperateTypeEnum.EXPORT;
 
@@ -89,4 +91,5 @@ public class AppAttendanceController {
         List<AttendanceExcelVO> datas = AttendanceConvert.INSTANCE.convertList02(list);
         ExcelUtils.write(response, "考勤打卡.xls", "数据", AttendanceExcelVO.class, datas);
     }
+
 }
