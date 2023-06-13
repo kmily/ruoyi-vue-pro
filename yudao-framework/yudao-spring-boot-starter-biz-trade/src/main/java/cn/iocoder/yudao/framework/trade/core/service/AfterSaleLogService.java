0330daf83e71a32607b852c19f8c1c75444aef2b
chenchen <934298133@qq.com>
2023-06-13 17:14:14 +0800

    交易日志封装

---
+++ yudao-framework/yudao-spring-boot-starter-biz-trade/src/main/java/cn/iocoder/yudao/framework/trade/core/service/AfterSaleLogService.java
@@ -0,0 +1,17 @@
+package cn.iocoder.yudao.framework.trade.core.service;
+
+import cn.iocoder.yudao.framework.trade.core.dto.TradeAfterSaleLogDTO;
+import org.springframework.scheduling.annotation.Async;
+
+public interface AfterSaleLogService {
+
+    /**
+     * 日志记录
+     *
+     * @param logDTO 日志记录
+     * @author 陈賝
+     * @date 2023/6/12 14:18
+     */
+    @Async
+    void insert(TradeAfterSaleLogDTO logDTO);
+}
