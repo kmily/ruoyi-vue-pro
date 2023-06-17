33e1e815a0716ca501952695f995ba9f2886613f
吴桥鑫 <1282047358@qq.com>
2023-06-17 13:47:30 +0800

    依赖解决

--- yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/mq/consumer/sms/SmsTemplateRefreshConsumer.java
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/mq/consumer/sms/SmsTemplateRefreshConsumer.java
@@ -17,7 +17,7 @@ import javax.annotation.Resource;
 @Slf4j
 public class SmsTemplateRefreshConsumer extends AbstractChannelMessageListener<SmsTemplateRefreshMessage> {
 
-    @Resource
+//    @Resource
     private SmsTemplateService smsTemplateService;
 
     @Override
