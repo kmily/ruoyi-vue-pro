6bb1b88f5bfa1a07ea0b8babe7e1e6a468d57adf
V2DB\sz <shaozhen_w@126.com>
2023-07-24 15:26:25 +0800

    shaozhen

---
+++ yudao-ui-admin/src/views/biz/calc/lx/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="app-container">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "CalcInterestRateData",
+  components: {
+  },
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 导出遮罩层
+      exportLoading: false,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 利率数据列表
+      list: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNo: 1,
+        pageSize: 10,
+        startDate: [],
+        rateHalfYear: null,
+        rateOneYear: null,
+        rateThreeYear: null,
+        rateFiveYear: null,
+        rateOverFiveYear: null,
+        createTime: [],
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+      }
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+
+  }
+
+};
+</script>
