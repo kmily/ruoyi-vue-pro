3c0bc5ae529465758de47c466258ebfcfcb1928e
wyzzyw <2960571342@qq.com>
2023-10-15 19:39:07 +0800

    fix: InEnum校验器不支持集合字段类型的校验

--- yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/validation/InEnumValidator.java
+++ yudao-framework/yudao-common/src/main/java/cn/iocoder/yudao/framework/common/validation/InEnumValidator.java
@@ -1,17 +1,19 @@
 package cn.iocoder.yudao.framework.common.validation;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.iocoder.yudao.framework.common.core.IntArrayValuable;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class InEnumValidator implements ConstraintValidator<InEnum, Integer> {
+public class InEnumValidator implements ConstraintValidator<InEnum, Object> {
 
-    private List<Integer> values;
+    private List<?> values;
 
     @Override
     public void initialize(InEnum annotation) {
@@ -24,15 +26,23 @@ public class InEnumValidator implements ConstraintValidator<InEnum, Integer> {
     }
 
     @Override
-    public boolean isValid(Integer value, ConstraintValidatorContext context) {
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
         // 为空时，默认不校验，即认为通过
         if (value == null) {
             return true;
         }
-        // 校验通过
-        if (values.contains(value)) {
-            return true;
+
+        //校验通过
+        if (value instanceof Collection<?>) {
+            if (CollUtil.containsAll(values, (Collection<?>) value)) {
+                return true;
+            }
+        } else {
+            if (CollUtil.contains(values, value)) {
+                return true;
+            }
         }
+
         // 校验不通过，自定义提示语句（因为，注解上的 value 是枚举类，无法获得枚举类的实际值）
         context.disableDefaultConstraintViolation(); // 禁用默认的 message 的值
         context.buildConstraintViolationWithTemplate(context.getDefaultConstraintMessageTemplate()
