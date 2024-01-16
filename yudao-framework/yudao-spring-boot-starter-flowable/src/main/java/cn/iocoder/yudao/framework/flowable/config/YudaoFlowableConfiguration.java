d0b2a77b30e0aa9d2e0d0df66355945f130af08e
JamesY <james.yyq@gmail.com>
2024-01-16 22:22:18 +0800

    修复同时开启bpm和pay模块时创建bpm的ThreadPoolTaskExecutor失败问题

--- yudao-framework/yudao-spring-boot-starter-flowable/src/main/java/cn/iocoder/yudao/framework/flowable/config/YudaoFlowableConfiguration.java
+++ yudao-framework/yudao-spring-boot-starter-flowable/src/main/java/cn/iocoder/yudao/framework/flowable/config/YudaoFlowableConfiguration.java
@@ -3,23 +3,22 @@ package cn.iocoder.yudao.framework.flowable.config;
 import cn.iocoder.yudao.framework.common.enums.WebFilterOrderEnum;
 import cn.iocoder.yudao.framework.flowable.core.web.FlowableWebFilter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.AsyncListenableTaskExecutor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @AutoConfiguration
 public class YudaoFlowableConfiguration {
 
     /**
-     * 参考 {@link org.flowable.spring.boot.FlowableJobConfiguration} 类，创建对应的 AsyncListenableTaskExecutor Bean
+     * 参考 {@link org.flowable.spring.boot.FlowableJobConfiguration} 类，创建对应的 AsyncTaskExecutor Bean
      *
      * 如果不创建，会导致项目启动时，Flowable 报错的问题
      */
     @Bean
-    @ConditionalOnMissingBean
-    public AsyncListenableTaskExecutor taskExecutor() {
+    @Primary
+    public org.springframework.core.task.AsyncTaskExecutor flowableAsyncTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(8);
         executor.setMaxPoolSize(8);
