beb734bcb1dee4e857bd6b7e5800aa3d3666ac03
glzaboy <glzaboy@163.com>
2024-01-30 16:18:30 +0800

    增加steam测试demo

---
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/admin/DemoTestController.java
@@ -0,0 +1,21 @@
+package cn.iocoder.yudao.module.steam.controller.admin;
+
+import cn.iocoder.yudao.framework.common.pojo.CommonResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "管理后台 - Test")
+@RestController
+@RequestMapping("/demo/test")
+@Validated
+public class DemoTestController {
+    @GetMapping("/get")
+    @Operation(summary = "获取 test 信息")
+    public CommonResult<String> get() {
+        return CommonResult.success("true");
+    }
+}
