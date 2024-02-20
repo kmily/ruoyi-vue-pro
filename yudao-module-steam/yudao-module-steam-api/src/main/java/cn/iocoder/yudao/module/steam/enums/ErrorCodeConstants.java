ccd63d1927d972f8774e90edb2a9d653e951707c
glzaboy <glzaboy@163.com>
2024-02-20 17:40:25 +0800

    增加提现后台功能

--- yudao-module-steam/yudao-module-steam-api/src/main/java/cn/iocoder/yudao/module/steam/enums/ErrorCodeConstants.java
+++ yudao-module-steam/yudao-module-steam-api/src/main/java/cn/iocoder/yudao/module/steam/enums/ErrorCodeConstants.java
@@ -36,5 +36,6 @@ public interface ErrorCodeConstants {
     ErrorCode INV_NOT_EXISTS = new ErrorCode(1_100_008_02, "steam用户库存储不存在");
     // ========== steam订单
     ErrorCode INV_ORDER_NOT_EXISTS = new ErrorCode(1_100_009_01, "steam订单不存在");
-
+    // ========== 提现
+    ErrorCode WITHDRAWAL_NOT_EXISTS = new ErrorCode(1_100_010_01, "提现不存在");
 }
