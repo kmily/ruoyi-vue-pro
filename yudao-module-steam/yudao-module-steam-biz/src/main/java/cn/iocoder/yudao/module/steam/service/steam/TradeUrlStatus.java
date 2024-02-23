c4bbc61c12bf9cba7c03d000ec85d4b7a4a5cabc
glzaboy <glzaboy@163.com>
2024-02-23 14:41:17 +0800

    开放平台增加交易链接检测

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/steam/TradeUrlStatus.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/steam/TradeUrlStatus.java
@@ -3,7 +3,7 @@ package cn.iocoder.yudao.module.steam.service.steam;
 public enum TradeUrlStatus {
     OK(0,"正常"),ERRORURL(1,"链接错误"),BUSY(3,"请稍后再试"),
     DISALLOW(4,"账号交易权限被封禁，无法交易"),NotAvailable(5,"该交易链接已不再可用"),
-    PRIVATE(7,"该账号个人资料私密无法交易"),LIMIT(8,"此用户帐户功能受限");
+    PRIVATE(7,"该账号个人资料私密无法交易"),LIMIT(8,"此用户帐户功能受限"),NOBOT(9,"你的帐号下没有有效steam帐号无法验证");
     private Integer status;
     private String message;
 
