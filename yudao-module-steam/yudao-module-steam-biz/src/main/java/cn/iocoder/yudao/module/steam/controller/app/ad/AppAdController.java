189d559836da7b1ddb7290db630c295c08e96418
glzaboy <glzaboy@163.com>
2024-02-27 19:04:33 +0800

    增加广告位接口

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/ad/AppAdController.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/ad/AppAdController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.annotation.security.PermitAll;
 import javax.validation.Valid;
 
 import java.util.List;
@@ -27,6 +28,7 @@ public class AppAdController {
 
     @PostMapping("/getAd")
     @Operation(summary = "饰品在售预览")
+    @PermitAll
     public CommonResult<List<AdDO>> getAd(@RequestBody @Valid AdBlockReqVO adBlockReqVO){
         List<AdDO> enableAdListByBlockId = adBlockService.getEnableAdListByBlockId(adBlockReqVO.getBlockId());
         return success(enableAdListByBlockId);
