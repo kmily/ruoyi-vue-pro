98fe85b20a7ebefdc38b3ec1fe1c86d794047938
glzaboy <glzaboy@163.com>
2024-02-28 16:16:25 +0800

    库存计划任务

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/job/UserInventoryRefreshJob.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/job/UserInventoryRefreshJob.java
@@ -2,6 +2,7 @@ package cn.iocoder.yudao.module.steam.job;
 
 import cn.iocoder.yudao.framework.quartz.core.handler.JobHandler;
 import cn.iocoder.yudao.framework.tenant.core.job.TenantJob;
+import cn.iocoder.yudao.framework.tenant.core.util.TenantUtils;
 import cn.iocoder.yudao.module.steam.dal.dataobject.binduser.BindUserDO;
 import cn.iocoder.yudao.module.steam.dal.mysql.binduser.BindUserMapper;
 import cn.iocoder.yudao.module.steam.service.SteamInvService;
@@ -32,19 +33,20 @@ public class UserInventoryRefreshJob implements JobHandler {
     @Override
     public String execute(String param) {
 
-        // 查询用户表 steam_bind_user 的 steamId
-        List<Long> bindUserIds = bindUserMapper.selectList()
+        String execute = TenantUtils.execute(1l, () -> {
+            List<Long> bindUserIds = bindUserMapper.selectList()
                 .stream()
                 .distinct()
                 .map(BindUserDO::getUserId)
                 .collect(Collectors.toList());
-        if (!bindUserIds.isEmpty()){
-            for (Long bindUserId : bindUserIds){
-                steamInvService.fetchInventory(bindUserId,"730");
+            if (!bindUserIds.isEmpty()){
+                for (Long bindUserId : bindUserIds){
+                    steamInvService.fetchInventory(bindUserId,"730");
+                }
             }
-        } else {
-            return "定时任务-用户库存查询为空";
-        }
-        return "定时任务-更新库存成功";
+
+            return "完成";
+        });
+        return String.format("执行关闭成功 %s 个", execute);
     }
 }
