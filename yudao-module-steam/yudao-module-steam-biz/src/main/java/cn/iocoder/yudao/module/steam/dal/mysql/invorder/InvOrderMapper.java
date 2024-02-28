4d7b0ce3b9b926474c81ef7a3445e873f48ee8d3
glzaboy <glzaboy@163.com>
2024-02-28 11:41:27 +0800

    重新生成invorder表

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/dal/mysql/invorder/InvOrderMapper.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/dal/mysql/invorder/InvOrderMapper.java
@@ -27,6 +27,7 @@ public interface InvOrderMapper extends BaseMapperX<InvOrderDO> {
                 .eqIfPresent(InvOrderDO::getTransferText, reqVO.getTransferText())
                 .eqIfPresent(InvOrderDO::getTransferStatus, reqVO.getTransferStatus())
                 .eqIfPresent(InvOrderDO::getPayOrderStatus, reqVO.getPayOrderStatus())
+                .eqIfPresent(InvOrderDO::getSellId, reqVO.getSellId())
                 .orderByDesc(InvOrderDO::getId));
     }
     default int updateByIdAndPayed(Long id, boolean wherePayed, InvOrderDO updateObj) {
