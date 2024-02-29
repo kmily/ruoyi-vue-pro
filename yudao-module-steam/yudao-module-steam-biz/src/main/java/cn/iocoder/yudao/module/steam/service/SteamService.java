47cd575922046e8bd21ee82ecab99352a2bc48db
glzaboy <glzaboy@163.com>
2024-02-29 18:38:22 +0800

    优化异步任务

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/SteamService.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/SteamService.java
@@ -1,5 +1,6 @@
 package cn.iocoder.yudao.module.steam.service;
 
+import cn.hutool.extra.spring.SpringUtil;
 import cn.iocoder.yudao.framework.common.enums.CommonStatusEnum;
 import cn.iocoder.yudao.framework.common.exception.ServiceException;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
@@ -140,7 +141,8 @@ public class SteamService {
         bindUserDO.setTradeUrl(steamWeb.getTreadUrl().get());
         bindUserDO.setSteamName(steamMaFile.getAccountName());
         bindUserMapper.updateById(bindUserDO);
-        firstGetInv(bindUserDO.getSteamId());
+        SteamService bean = SpringUtil.getBean(this.getClass());
+        bean.firstGetInv(bindUserDO.getSteamId());
     }
 
 
