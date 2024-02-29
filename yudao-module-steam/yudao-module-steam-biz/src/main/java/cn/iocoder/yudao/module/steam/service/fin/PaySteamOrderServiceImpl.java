1b875fdcf762838c0ef5e703724e60aca5f229b9
glzaboy <glzaboy@163.com>
2024-02-29 11:24:49 +0800

    库存订单中增加invdescID

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/fin/PaySteamOrderServiceImpl.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/fin/PaySteamOrderServiceImpl.java
@@ -214,7 +214,7 @@ public class PaySteamOrderServiceImpl implements PaySteamOrderService {
         InvOrderDO invOrderDO = new InvOrderDO().setSellId(createReqVO.getSellId()).setSteamId(createReqVO.getSteamId())
                 .setPrice(0).setSteamId(createReqVO.getSteamId())
                 .setPayOrderStatus(PayOrderStatusEnum.WAITING.getStatus())
-                .setTransferText(new TransferMsg())
+                .setTransferText(new TransferMsg()).setInvDescId(sellingDO.getInvDescId())
                 .setPayStatus(false).setRefundPrice(0).setUserId(loginUser.getId()).setUserType(loginUser.getUserType());
         validateInvOrderCanCreate(invOrderDO);
         invOrderMapper.insert(invOrderDO);
