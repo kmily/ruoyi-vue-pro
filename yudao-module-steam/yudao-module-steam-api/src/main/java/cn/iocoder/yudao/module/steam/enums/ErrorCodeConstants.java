bbcb6e11b2da71d0a0aca9f3b28a2863a0153a91
glzaboy <glzaboy@163.com>
2024-03-04 15:11:17 +0800

    提交youyou订单后台管理

--- yudao-module-steam/yudao-module-steam-api/src/main/java/cn/iocoder/yudao/module/steam/enums/ErrorCodeConstants.java
+++ yudao-module-steam/yudao-module-steam-api/src/main/java/cn/iocoder/yudao/module/steam/enums/ErrorCodeConstants.java
@@ -70,5 +70,8 @@ public interface ErrorCodeConstants {
     // ========== 广告位 TODO 补充编号 ==========
     ErrorCode AD_BLOCK_NOT_EXISTS = new ErrorCode(1_100_011_01, "广告位不存在");
 
+    // ========== steam订单 TODO 补充编号 ==========
+    ErrorCode YOUYOU_ORDER_NOT_EXISTS = new ErrorCode(1_100_012_01, "steam订单不存在");
+
 
 }
