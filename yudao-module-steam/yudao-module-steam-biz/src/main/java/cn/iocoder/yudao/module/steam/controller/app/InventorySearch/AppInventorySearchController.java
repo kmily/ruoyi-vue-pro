3d9a8e5ea65f10f74c9403498a6df79c655542c1
glzaboy <glzaboy@163.com>
2024-03-07 18:14:22 +0800

    优化库存代码

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/InventorySearch/AppInventorySearchController.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/InventorySearch/AppInventorySearchController.java
@@ -21,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -79,7 +80,8 @@ public class AppInventorySearchController {
     //
     @GetMapping("/after_SearchFromSteam")
     @Operation(summary = "查询数据库的库存数据")
-    public void SearchFromSteam() throws JsonProcessingException {
-        steamInvService.FistGetInventory(32L,"730");}
+    public void SearchFromSteam(@RequestParam Long id) throws JsonProcessingException {
+        steamInvService.FistGetInventory(id,"730");
+    }
 }
 
