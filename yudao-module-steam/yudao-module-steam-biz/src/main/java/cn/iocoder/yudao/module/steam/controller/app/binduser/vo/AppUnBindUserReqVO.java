69fcb235f5f8319549782839467d87e0268e1f44
glzaboy <glzaboy@163.com>
2024-03-08 11:28:57 +0800

    完成解绑用户接口

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/binduser/vo/AppUnBindUserReqVO.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/binduser/vo/AppUnBindUserReqVO.java
@@ -1,11 +1,8 @@
 package cn.iocoder.yudao.module.steam.controller.app.binduser.vo;
 
-import cn.iocoder.yudao.framework.common.pojo.PageParam;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.ToString;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotNull;
 
