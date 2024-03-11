6ceded0f46d11c16e8f9083c99e0bb3915c10985
glacier <13108108+leea0921@user.noreply.gitee.com>
2024-03-11 15:32:50 +0800

    search接口返回多条数据和总数修改

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/selling/SellingsearchService.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/selling/SellingsearchService.java
@@ -1,11 +1,13 @@
 package cn.iocoder.yudao.module.steam.service.selling;
 
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
+import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
+import cn.iocoder.yudao.framework.security.core.LoginUser;
+import cn.iocoder.yudao.framework.security.core.util.SecurityFrameworkUtils;
 import cn.iocoder.yudao.module.steam.controller.admin.invdesc.vo.InvDescPageReqVO;
 import cn.iocoder.yudao.module.steam.controller.admin.selling.vo.SellingPageReqVO;
 import cn.iocoder.yudao.module.steam.controller.admin.selling.vo.SellingRespVO;
 import cn.iocoder.yudao.module.steam.controller.app.InventorySearch.vo.AppInvPageReqVO;
-import cn.iocoder.yudao.module.steam.dal.dataobject.inv.InvDO;
 import cn.iocoder.yudao.module.steam.dal.dataobject.invdesc.InvDescDO;
 import cn.iocoder.yudao.module.steam.dal.dataobject.selling.SellingDO;
 import cn.iocoder.yudao.module.steam.dal.mysql.invdesc.InvDescMapper;
@@ -28,12 +30,13 @@ public class SellingsearchService {
     private SellingMapper sellingMapper;
 
     @Resource
+    private SellingService sellingService;
+
+    @Resource
     private InvDescMapper invDescMapper;
 
     @Resource
     private InvDescService invDescService;
-    @Resource
-    private SellingService sellingService;
 
 
     public PageResult<SellingRespVO> sellingPageSearch(SellingPageReqVO pageReqVO) {
@@ -43,24 +46,26 @@ public class SellingsearchService {
         invDescPageReqVO.setClassid(pageReqVO.getClassid());
         invDescPageReqVO.setInstanceid(pageReqVO.getInstanceid());
         PageResult<InvDescDO> invDescDOPageResult = invDescMapper.selectPage(invDescPageReqVO);
-        SellingRespVO sellingRespVO = new SellingRespVO();
-        for(SellingDO s : collect){
-            sellingRespVO.setId(s.getId());
-            sellingRespVO.setInvId(s.getInvId());
-            sellingRespVO.setInvDescId(s.getInvDescId());
-            sellingRespVO.setAssetid(s.getAssetid());
-            sellingRespVO.setClassid(s.getClassid());
-            sellingRespVO.setInstanceid(s.getInstanceid());
-            sellingRespVO.setSteamId(s.getSteamId());
-            sellingRespVO.setStatus(s.getStatus());
-            // 价格
-            sellingRespVO.setPrice(s.getPrice());
-            // 图片
-            sellingRespVO.setIconUrl(invDescDOPageResult.getList().get(0).getIconUrl());
-            // 武器名称
-            sellingRespVO.setMarketName(invDescDOPageResult.getList().get(0).getMarketName());
-        }
-        return new PageResult<>(Collections.singletonList(sellingRespVO), invPage.getTotal());
 
+        List<SellingRespVO> sellingRespVOList = new ArrayList<>();
+        for(SellingDO s : collect){
+            SellingRespVO tempSellingRespVO = new SellingRespVO();
+            tempSellingRespVO.setId(s.getId());
+            tempSellingRespVO.setInvId(s.getInvId());
+            tempSellingRespVO.setInvDescId(s.getInvDescId());
+            tempSellingRespVO.setAssetid(s.getAssetid());
+            tempSellingRespVO.setClassid(s.getClassid());
+            tempSellingRespVO.setInstanceid(s.getInstanceid());
+            tempSellingRespVO.setSteamId(s.getSteamId());
+            tempSellingRespVO.setStatus(s.getStatus());
+            // 价格
+            tempSellingRespVO.setPrice(s.getPrice());
+            // 图片
+            tempSellingRespVO.setIconUrl(invDescDOPageResult.getList().get(0).getIconUrl());
+            // 武器名称
+            tempSellingRespVO.setMarketName(invDescDOPageResult.getList().get(0).getMarketName());
+            sellingRespVOList.add(tempSellingRespVO);
+        }
+        return new PageResult<>(sellingRespVOList, invPage.getTotal());
     }
 }
