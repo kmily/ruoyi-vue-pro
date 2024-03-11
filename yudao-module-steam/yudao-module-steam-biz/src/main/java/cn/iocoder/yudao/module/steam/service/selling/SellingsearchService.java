0b3cc204b27bf6b9ba537e3273aaee073ba12cbd
glacier <13108108+leea0921@user.noreply.gitee.com>
2024-03-11 14:46:35 +0800

    饰品在售预览修改返回total

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/selling/SellingsearchService.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/selling/SellingsearchService.java
@@ -4,14 +4,19 @@ import cn.iocoder.yudao.framework.common.pojo.PageResult;
 import cn.iocoder.yudao.module.steam.controller.admin.invdesc.vo.InvDescPageReqVO;
 import cn.iocoder.yudao.module.steam.controller.admin.selling.vo.SellingPageReqVO;
 import cn.iocoder.yudao.module.steam.controller.admin.selling.vo.SellingRespVO;
+import cn.iocoder.yudao.module.steam.controller.app.InventorySearch.vo.AppInvPageReqVO;
+import cn.iocoder.yudao.module.steam.dal.dataobject.inv.InvDO;
 import cn.iocoder.yudao.module.steam.dal.dataobject.invdesc.InvDescDO;
 import cn.iocoder.yudao.module.steam.dal.dataobject.selling.SellingDO;
 import cn.iocoder.yudao.module.steam.dal.mysql.invdesc.InvDescMapper;
 import cn.iocoder.yudao.module.steam.dal.mysql.selling.SellingMapper;
+import cn.iocoder.yudao.module.steam.service.invdesc.InvDescService;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,14 +30,19 @@ public class SellingsearchService {
     @Resource
     private InvDescMapper invDescMapper;
 
+    @Resource
+    private InvDescService invDescService;
+    @Resource
+    private SellingService sellingService;
 
-    public SellingRespVO sellingPageSearch(SellingPageReqVO pageReqVO) {
+
+    public PageResult<SellingRespVO> sellingPageSearch(SellingPageReqVO pageReqVO) {
         List<SellingDO> collect = sellingMapper.selectPage(pageReqVO).getList().stream().distinct().collect(Collectors.toList());
+        PageResult<SellingDO> invPage = sellingService.getSellingPage(pageReqVO);
         InvDescPageReqVO invDescPageReqVO = new InvDescPageReqVO();
         invDescPageReqVO.setClassid(pageReqVO.getClassid());
         invDescPageReqVO.setInstanceid(pageReqVO.getInstanceid());
         PageResult<InvDescDO> invDescDOPageResult = invDescMapper.selectPage(invDescPageReqVO);
-//        Long invDescDOPageResult.getList().get(0).getIconUrl();
         SellingRespVO sellingRespVO = new SellingRespVO();
         for(SellingDO s : collect){
             sellingRespVO.setId(s.getId());
@@ -50,6 +60,7 @@ public class SellingsearchService {
             // 武器名称
             sellingRespVO.setMarketName(invDescDOPageResult.getList().get(0).getMarketName());
         }
-        return sellingRespVO;
+        return new PageResult<>(Collections.singletonList(sellingRespVO), invPage.getTotal());
+
     }
 }
