d10747c8234643fdb4157bee6001ea8c795f00fd
glzaboy <glzaboy@163.com>
2024-03-13 09:54:15 +0800

    在售商品列表

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/droplist/vo/ItemResp.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/droplist/vo/ItemResp.java
@@ -1,10 +1,12 @@
 package cn.iocoder.yudao.module.steam.controller.app.droplist.vo;
 
 import cn.iocoder.yudao.module.steam.service.steam.C5ItemInfo;
+import cn.iocoder.yudao.module.steam.service.steam.C5ItemTag;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -16,4 +18,6 @@ public class ItemResp implements Serializable {
     private String imageUrl;
     private C5ItemInfo itemInfo;
     private String autoPrice;
+    private String sellPrice;
+    private List<C5ItemTag> tagList;
 }
