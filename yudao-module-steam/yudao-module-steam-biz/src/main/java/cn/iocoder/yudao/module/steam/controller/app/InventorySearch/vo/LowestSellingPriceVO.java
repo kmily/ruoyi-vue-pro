c1b99769ac0ab48cb35f019ce182d72ce84db66f
lgmpuls <1127560429@qq.com>
2024-03-15 20:10:41 +0800

    库存更新

---
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/InventorySearch/vo/LowestSellingPriceVO.java
@@ -0,0 +1,19 @@
+package cn.iocoder.yudao.module.steam.controller.app.InventorySearch.vo;
+
+import lombok.Data;
+
+@Data
+public class LowestSellingPriceVO {
+
+    /**
+     * 饰品最低售价
+     */
+    private String lowestSellingPrice;
+
+    /**
+     * 饰品名称
+     */
+    private String marketHashName;
+
+
+}
