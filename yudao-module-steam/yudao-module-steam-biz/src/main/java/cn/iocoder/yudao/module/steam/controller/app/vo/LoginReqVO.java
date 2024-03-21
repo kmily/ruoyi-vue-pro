974b1a4e6f1c757adc4d4d0dca6b883b7bba02fd
glzaboy <glzaboy@163.com>
2024-03-21 16:38:20 +0800

    删除不需要的代码

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/vo/LoginReqVO.java
+++
@@ -1,9 +0,0 @@
-package cn.iocoder.yudao.module.steam.controller.app.vo;
-
-import cn.iocoder.yudao.module.steam.service.selrarity.SelRarityService;
-import lombok.Data;
-
-@Data
-public class LoginReqVO {
-    private Long userid;
-}
