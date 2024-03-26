1a840ec702bec70d18223d5a117924ff5bc495e6
glzaboy <glzaboy@163.com>
2024-03-26 15:25:45 +0800

    完成订单下单功能

--- yudao-module-pay/yudao-module-pay-biz/src/main/java/cn/iocoder/yudao/module/pay/service/wallet/PayWalletServiceImpl.java
+++ yudao-module-pay/yudao-module-pay-biz/src/main/java/cn/iocoder/yudao/module/pay/service/wallet/PayWalletServiceImpl.java
@@ -132,6 +132,8 @@ public class PayWalletServiceImpl implements  PayWalletService {
         int updateCounts;
         switch (bizType) {
             case PAYMENT:
+            case SUB_INV_SERVICE_FEE:
+            case SUB_STEAM_CASH:
             case STEAM_PAY: {
                 updateCounts = walletMapper.updateWhenConsumption(payWallet.getId(), price);
                 break;
