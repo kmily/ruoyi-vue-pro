e97a4c39ea60e9fd73ea3ee31126feb085c59f76
lgmpuls <1127560429@qq.com>
2024-04-01 17:28:56 +0800

    RSA校验

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/devaccount/AppDevAccountController.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/controller/app/devaccount/AppDevAccountController.java
@@ -2,6 +2,12 @@ package cn.iocoder.yudao.module.steam.controller.app.devaccount;
 
 import cn.hutool.core.util.IdUtil;
 import cn.iocoder.yudao.framework.common.pojo.CommonResult;
+import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
+
+import cn.iocoder.yudao.module.member.controller.app.auth.vo.AppAuthSmsValidateReqVO;
+import cn.iocoder.yudao.module.member.dal.dataobject.user.MemberUserDO;
+import cn.iocoder.yudao.module.member.dal.mysql.user.MemberUserMapper;
+import cn.iocoder.yudao.module.member.service.auth.MemberAuthService;
 import cn.iocoder.yudao.module.steam.controller.app.devaccount.vo.AppDevAccountSaveReqVO;
 import cn.iocoder.yudao.module.steam.dal.dataobject.devaccount.DevAccountDO;
 import cn.iocoder.yudao.module.steam.service.devaccount.DevAccountService;
@@ -16,6 +22,8 @@ import java.util.List;
 
 import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
 
+import static cn.iocoder.yudao.framework.security.core.util.SecurityFrameworkUtils.getLoginUserId;
+
 @Tag(name = "steam后台 - 开放平台用户")
 @RestController
 @RequestMapping("/steam-app/dev-account")
@@ -25,9 +33,34 @@ public class AppDevAccountController {
     @Resource
     private DevAccountService devAccountService;
 
+//    @Resource
+//    private AppAuthSmsValidateReqVO appAuthSmsValidateReqVO;
+    @Resource
+    private MemberAuthService authService;
+
+    @Resource
+    private MemberUserMapper memberUserMapper;
+
+//    @Resource
+//    private SecurityFrameworkUtils securityFrameworkUtils ;
+
+
+
+
     @PostMapping("/create")
     @Operation(summary = "创建开放平台用户")
     public CommonResult<String> createDevAccount(@RequestBody @Valid AppDevAccountSaveReqVO reqVO) {
+        reqVO.setUserName("open_" + IdUtil.simpleUUID());
+        return success(devAccountService.apply(reqVO));
+    }
+
+    @PostMapping("/update")
+    @Operation(summary = "开放平台用户修改RSA")
+    public CommonResult<String> createDevAccount1(@RequestBody @Valid AppAuthSmsValidateReqVO reqVO1,@RequestBody @Valid AppDevAccountSaveReqVO reqVO) {
+        List<MemberUserDO> memberUserDOS = memberUserMapper.selectList(new LambdaQueryWrapperX<MemberUserDO>().eq(MemberUserDO::getId, getLoginUserId()));
+        reqVO1.setMobile(memberUserDOS.get(0).getMobile());
+        // 校验手机验证码
+        authService.validateSmsCode(getLoginUserId(), reqVO1);
         reqVO.setUserName("open_" + IdUtil.simpleUUID());
         return success(devAccountService.apply(reqVO));
     }
