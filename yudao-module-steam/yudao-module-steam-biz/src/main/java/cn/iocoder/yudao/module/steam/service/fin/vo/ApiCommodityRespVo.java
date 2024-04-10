e163038cae13909d969ed5d375b40818a4d3799b
glzaboy <glzaboy@163.com>
2024-04-10 19:03:02 +0800

    修改基础架构

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/fin/vo/ApiCommodityRespVo.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/fin/vo/ApiCommodityRespVo.java
@@ -1,12 +1,15 @@
 package cn.iocoder.yudao.module.steam.service.fin.vo;
 
 import cn.iocoder.yudao.module.steam.enums.PlatCodeEnum;
+import lombok.Data;
 
 import java.io.Serializable;
 
+@Data
 public class ApiCommodityRespVo implements Serializable {
     /**
      * 商品价格
+     * 单位分
      */
     private Integer price;
     /**
