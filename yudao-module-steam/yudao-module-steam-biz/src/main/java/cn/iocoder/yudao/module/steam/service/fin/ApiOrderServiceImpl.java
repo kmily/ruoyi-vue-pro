2de2dba86a3fa76a61b41757856e011846043ad7
glzaboy <glzaboy@163.com>
2024-04-12 15:16:10 +0800

    增加购买方式

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/fin/ApiOrderServiceImpl.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/fin/ApiOrderServiceImpl.java
@@ -163,6 +163,7 @@ public class ApiOrderServiceImpl implements ApiOrderService {
         ApiOrderDO orderDO=new ApiOrderDO()
                 //设置买家
                 .setBuyUserId(loginUser.getId()).setBuyUserType(loginUser.getUserType())
+                .setBuyMethod(reqVo.getPlatform().getCode())
                 .setBuyBindUserId(buyBindUserDO.getId()).setBuySteamId(buyBindUserDO.getSteamId()).setBuyTradeLinks(buyBindUserDO.getTradeUrl())
                 //设置卖家信息
                 //服务费账号
