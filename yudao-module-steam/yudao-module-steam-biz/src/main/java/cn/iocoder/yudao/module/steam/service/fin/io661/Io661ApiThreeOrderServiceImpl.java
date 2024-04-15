abb83726e76131d3bcd172ec87d10286c4a4f294
glzaboy <glzaboy@163.com>
2024-04-15 13:39:37 +0800

    创建订单完成增加相关数据返回

--- yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/fin/io661/Io661ApiThreeOrderServiceImpl.java
+++ yudao-module-steam/yudao-module-steam-biz/src/main/java/cn/iocoder/yudao/module/steam/service/fin/io661/Io661ApiThreeOrderServiceImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
 
-@Service
+//@Service
 @Slf4j
 public class Io661ApiThreeOrderServiceImpl implements ApiThreeOrderService {
     @Resource
