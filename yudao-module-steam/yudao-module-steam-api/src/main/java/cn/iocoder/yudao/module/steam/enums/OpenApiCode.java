d729fed2a32b2f6155d8ce75494613e677f54c19
Ning <12975239+AmbitionHJH@user.noreply.gitee.com>
2024-04-18 14:36:13 +0800

    V5登录修改

--- yudao-module-steam/yudao-module-steam-api/src/main/java/cn/iocoder/yudao/module/steam/enums/OpenApiCode.java
+++ yudao-module-steam/yudao-module-steam-api/src/main/java/cn/iocoder/yudao/module/steam/enums/OpenApiCode.java
@@ -44,7 +44,7 @@ public interface OpenApiCode {
     ErrorCode ERR_1002 = new ErrorCode(1002, "商品状态发生变化，暂无法购买，请选购其他商品");
 
     //v5登录
-    ErrorCode ERR_V5_LOGIN = new ErrorCode(1003, "请求v5登录接口出错 ");
+    ErrorCode ERR_V5_LOGIN = new ErrorCode(1003, "请求login接口出错，请联系管理员 ");
     ErrorCode ERR_WIFI = new ErrorCode(1004, "网络连接不良，请重试 ");
 
 
