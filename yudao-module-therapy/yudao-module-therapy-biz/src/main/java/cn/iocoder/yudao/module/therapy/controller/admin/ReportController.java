b508fc193d5763abca219f38e1caed21885f4939
lei.cao <lei.cao@quantgroup.com>
2024-06-26 18:07:50 +0800

    B端报告接口

---
+++ yudao-module-therapy/yudao-module-therapy-biz/src/main/java/cn/iocoder/yudao/module/therapy/controller/admin/ReportController.java
@@ -0,0 +1,100 @@
+package cn.iocoder.yudao.module.therapy.controller.admin;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.json.JSONObject;
+import cn.iocoder.boot.module.therapy.enums.SurveyType;
+import cn.iocoder.yudao.framework.common.core.KeyValue;
+import cn.iocoder.yudao.framework.common.pojo.CommonResult;
+import cn.iocoder.yudao.module.therapy.controller.app.vo.AnAnswerReqVO;
+import cn.iocoder.yudao.module.therapy.controller.app.vo.SubmitSurveyReqVO;
+import cn.iocoder.yudao.module.therapy.dal.dataobject.survey.AnswerDetailDO;
+import cn.iocoder.yudao.module.therapy.dal.dataobject.survey.SurveyAnswerDO;
+import cn.iocoder.yudao.module.therapy.service.StatService;
+import cn.iocoder.yudao.module.therapy.service.SurveyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static cn.iocoder.boot.module.therapy.enums.ErrorCodeConstants.SURVEY_ANSWER_NOT_EXISTS;
+import static cn.iocoder.yudao.framework.common.exception.util.ServiceExceptionUtil.exception;
+import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
+
+@Tag(name = "B端 - 数据及报告")
+@RestController
+@RequestMapping("/therapy/report")
+@Validated
+public class ReportController {
+    @Resource
+    private SurveyService surveyService;
+    @Resource
+    private StatService statService;
+    @GetMapping(value = "/scaleChart")
+    @Operation(summary = "量表报告拆线图数据")
+    @Parameter(name = "userId", description = "患者id", required = true, example = "104")
+    public CommonResult<Map<String, List<KeyValue>>> scaleChart(@RequestParam("userId") Long userId) {
+        Map<String, List<KeyValue>> res = statService.getScaleChart(userId);
+
+        return success(res);
+    }
+
+    @GetMapping("/getGoalMotive")
+    @Operation(summary = "获取目标与动机详情")
+    @Parameter(name = "userId", description = "患者id", required = true, example = "104")
+    public CommonResult<SubmitSurveyReqVO> getGoalMotive(@RequestParam("userId") Long userId) {
+        return success(surveyService.getGoalMotive(userId));
+    }
+
+    @GetMapping(value = "/getCommonDetail")
+    @Operation(summary = "获取通用详情")
+    @Parameter(name = "id", description = "报告id", required = true, example = "1024")
+    public CommonResult<SubmitSurveyReqVO> getCommonDetail(@RequestParam("id") Long id) {
+        SubmitSurveyReqVO res = new SubmitSurveyReqVO();
+        SurveyAnswerDO answerDO = surveyService.getAnswerDO(id);
+        if (Objects.isNull(answerDO)) {
+            throw exception(SURVEY_ANSWER_NOT_EXISTS);
+        }
+        List<AnswerDetailDO> detailDOS = surveyService.getAnswerDetailByAnswerId(id);
+        res.setId(id);
+        res.setSurveyType(answerDO.getSurveyType());
+        res.setQstList(new ArrayList<>());
+        for (AnswerDetailDO item : detailDOS) {
+            AnAnswerReqVO vo = new AnAnswerReqVO();
+            vo.setAnswer(item.getAnswer());
+            vo.setQstCode(item.getBelongQstCode());
+            res.getQstList().add(vo);
+        }
+        return success(res);
+    }
+
+    @GetMapping(value = "/moodScoring")
+    @Operation(summary = "每日心情评分拆线图数据")
+    @Parameter(name = "begin", description = "开始日期:yyyy-MM-dd", required = true, example = "2024-06-01")
+    @Parameter(name = "end", description = "结束日期:yyyy-MM-dd", required = true, example = "2024-06-01")
+    @Parameter(name = "userId", description = "患者id", required = true, example = "104")
+    public CommonResult<List<KeyValue>> moodScoring(@RequestParam("begin") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate begin
+            , @RequestParam("end")@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end
+    ,@RequestParam("userId") Long userId) {
+        List<SurveyAnswerDO> answerDOS = statService.getAnswerList(userId, begin, end, Arrays.asList(SurveyType.MOOD_MARK.getType()));
+        List<KeyValue> res = new ArrayList<>();
+        if (CollectionUtil.isNotEmpty(answerDOS)) {
+            for (SurveyAnswerDO item : answerDOS) {
+                KeyValue<LocalDateTime, Integer> keyValue = new KeyValue<>();
+                keyValue.setKey(item.getCreateTime());
+                keyValue.setValue((new JSONObject(item.getReprot())).getInt("score", 0));
+                res.add(keyValue);
+            }
+        }
+        return success(res);
+    }
+}
