fa5af7a740b9cf1d9e456fe4e4fb469d4fa7a477
lei.cao <lei.cao@quantgroup.com>
2024-07-01 18:24:47 +0800

    优化报告显示

--- yudao-module-member/yudao-module-member-api/src/main/java/cn/iocoder/yudao/module/member/api/user/dto/MemberUserPageReqDTO.java
+++ yudao-module-member/yudao-module-member-api/src/main/java/cn/iocoder/yudao/module/member/api/user/dto/MemberUserPageReqDTO.java
@@ -1,9 +1,19 @@
 package cn.iocoder.yudao.module.member.api.user.dto;
 
 import cn.iocoder.yudao.framework.common.pojo.PageParam;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
+@Schema(description = "管理后台 - 参数配置分页 Request VO")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class MemberUserPageReqDTO extends PageParam {
+    @Schema(description = "手机号", example = "15601691300")
     private String mobile;
 
+    @Schema(description = "用户昵称", example = "李四")
     private String nickname;
 }
