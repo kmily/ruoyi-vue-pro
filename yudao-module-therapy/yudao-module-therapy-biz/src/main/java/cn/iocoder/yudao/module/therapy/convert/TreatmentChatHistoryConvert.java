0608b1f4765388759900e55fd65ddd1c41dfdd6e
shrik <boobmoom@gmail.com>
2024-07-07 09:57:25 +0800

    fix

--- yudao-module-therapy/yudao-module-therapy-biz/src/main/java/cn/iocoder/yudao/module/therapy/convert/TreatmentChatHistoryConvert.java
+++ yudao-module-therapy/yudao-module-therapy-biz/src/main/java/cn/iocoder/yudao/module/therapy/convert/TreatmentChatHistoryConvert.java
@@ -1,14 +1,10 @@
 package cn.iocoder.yudao.module.therapy.convert;
 
-import cn.iocoder.boot.module.therapy.enums.TaskType;
 import cn.iocoder.yudao.module.therapy.controller.app.vo.TreatmentHistoryChatMessageVO;
 import cn.iocoder.yudao.module.therapy.dal.dataobject.definition.TreatmentChatHistoryDO;
-import cn.iocoder.yudao.module.therapy.dal.dataobject.definition.TreatmentDayitemInstanceDO;
-import javafx.concurrent.Task;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class TreatmentChatHistoryConvert {
     public static List<TreatmentHistoryChatMessageVO> convert(List<TreatmentChatHistoryDO> list) {
